a4d0543ff3fe5a71ae210d03670bc7c53b2d58c0
Dariya <dariyab@nycstudents.net>
2023-10-02 19:28:11 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const b1=document.getElementById("b1");
+const b2=document.getElementById("b2");
+
+b1.addEventListener("click", rollDice);
+
+function rollDice(){
+    const rand1= Math.floor(Math.random() * (6) + 1);
+    const rand2=Math.floor(Math.random() * (6) + 1);
+    b1.textContent = rand1;
+    b2.textContent = rand2;
+}
