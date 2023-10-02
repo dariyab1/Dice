83ffb45053724eac53621900ff090004996cc40b
Dariya <dariyab@nycstudents.net>
2023-10-02 19:32:39 -0400

    done

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const b1=document.getElementById("b1");
 const b2=document.getElementById("b2");
+const p1=document.getElementById("result");
 
 b1.addEventListener("click", rollDice);
 
@@ -8,4 +9,5 @@ function rollDice(){
     const rand2=Math.floor(Math.random() * (6) + 1);
     b1.textContent = rand1;
     b2.textContent = rand2;
+    p1.textContent = "Total is "+(rand1+rand2);
 }
